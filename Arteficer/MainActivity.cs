0c03681abb36f77d58cd5a974e8abe50f6cca87b
Biest1155 <mikk934a@edu.eal.dk>
2019-06-07 15:53:54 +0200

    Made it fucking start

--- Arteficer/MainActivity.cs
+++ Arteficer/MainActivity.cs
@@ -3,17 +3,50 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Arteficer.Models;
+using Android.Content;
 
 namespace Arteficer
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Repository repository = Repository.GetInstance();
+        ListView listview;
+        EditText name;
+        EditText type;
+        EditText element;
+        Switch setpiece;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity_main);                  // Set our view from the "main" layout resource
+
+            Button search = FindViewById<Button>(Resource.Id.searchButton);
+
+            Button random = FindViewById<Button>(Resource.Id.randomButton);
+
+            Button create = FindViewById<Button>(Resource.Id.createButton); // Make connection to button
+            create.Click += delegate {                                       // Make desired effect when clicked
+                Intent intent = new Intent(this, typeof(CreateActivity));
+                StartActivity(intent);
+            };
+
+            listview = FindViewById<ListView>(Resource.Id.listView1);
+            listview.Adapter = new ArtefactAdapter(this, repository.Artefacts);
+
+            name = FindViewById<EditText>(Resource.Id.nameEdit);
+            type = FindViewById<EditText>(Resource.Id.typeEdit);
+            element = FindViewById<EditText>(Resource.Id.elementEdit);
+            setpiece = FindViewById<Switch>(Resource.Id.setpieceSwitch);
         }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+        }
+
+
     }
 }
